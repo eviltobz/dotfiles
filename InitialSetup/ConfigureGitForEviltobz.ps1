d79d53e75405ed2c8695b2082c87391f057f212c
eviltobz <eviltobz@hotmail.com>
2012-04-09 08:07:21 +0000

    checking existance of ssh key

--- InitialSetup/ConfigureGitForEviltobz.ps1
+++ InitialSetup/ConfigureGitForEviltobz.ps1
@@ -1,4 +1,10 @@
+.\ConfigureBaseGit.ps1
+
 git config --global user.name "eviltobz"
 git config --global user.email "eviltobz@hotmail.com"
 
-.\ConfigureBaseGit.ps1
+$sshKeyPath = "$home\.ssh\id_rsa"
+if(!$(Test-Path $sshKeyPath))
+{
+  echo "Can't find $sshKeyPath"
+}
