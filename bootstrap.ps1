89e4851d8212800c8bc82c1e5362777f12e2f6ea
eviltobz <eviltobz@hotmail.com>
2018-05-18 09:25:21 +0100

    Pulling a bootstrap script out of the windows install - do minimal work to git clone the dotfiles dir, so it can do the main setup within the source-controlled folder

---
+++ bootstrap.ps1
@@ -0,0 +1,47 @@
+
+function ensureFolder ($path) {
+  if(-not (Test-Path $path)) {
+    mkdir $path
+  }
+}
+function gitGet ($name, $dest, $source) {
+  Write-Host "Pulling $name from $source into $dest"
+  git clone $source $dest
+}
+$chocInstallCode = 0
+function chocoInstall($name) {
+  if((choco list -l | findstr -i "^$name ").Count > 0) {
+    Write-Host "Choco has already installed $name"
+    choco list -l | findstr -i "^$name "
+    $chocInstallCode = 1
+  } else {
+    choco install $name --confirm
+    $chocInstallCode = 0
+  }
+}
+
+
+function DoPrereqs() {
+  if(-not (Test-Path "C:\ProgramData\chocolatey\bin")) {
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  }
+  chocoInstall git
+  $env:path += ";c:\Program Files\Git\cmd"
+
+  ensureFolder $args[0]
+  gitGet DotFiles $args[0] https://github.com/eviltobz/dotfiles.git 
+}
+
+if ((get-executionpolicy) -ne "Bypass") {
+  Write-Host "This machine is not set up to allow script execution. Its current policy is: $(Get-ExecutionPolicy)" -f red
+  Write-Host "Attempting to alter policy." -f yellow
+  Set-ExecutionPolicy Bypass
+}
+
+if ((get-executionpolicy) -eq "Bypass") {
+  DoPrereqs
+} else {
+  Write-Host "Could not alter execution policy. The current policy is: $(Get-ExecutionPolicy)" -f red
+  Write-Host "Please run the following command:" -f red
+  Write-Host "Set-ExecutionPolicy Bypass" -f yellow
+}
