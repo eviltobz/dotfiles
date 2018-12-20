479131aadd85ba42b2a3a77440c9f8c9a5d26012
toby carter <toby.carter@15below.com>
2018-12-20 16:20:17 +0000

    Windows install script

--- Install/install.ps1
+++ Install/install.ps1
@@ -5,10 +5,22 @@
 
 # make the script appropriately idempotent
 
+
+# get the starting path
+$current = pwd
+echo "Current path is $pwd"
+if($current.ToString().ToUpper().EndsWith("INSTALL")){
+  cd ..
+}
+$dotfiles = pwd
+
+
+
 function linkFile ($file, $dest) {
   if(-not (Test-Path $dest)) {
     write-host "Linking $file to $dest"
-    cmd /c mklink /h $dest $file
+#cmd /c mklink /h $dest $file
+    cmd /c mklink $dest $file
   }
 }
 
@@ -62,6 +74,22 @@ function chocoToTaskbar($name, $folder, $exe) {
   }
 }
 
+function startupAutoHotKeyScript($name, $source, $icon) {
+  $ahk = "C:\Program Files\AutoHotkey\AutoHotKey.exe" 
+  $dest = "$($env:userprofile)\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$name.lnk"
+  if(-not (Test-Path $dest)) {
+    Write-Host "Creating startup item $name from $source"
+    Write-Host "Creating in $dest"
+    Write-Host "ahk in $ahk"
+
+    $s=(New-Object -COM WScript.Shell).CreateShortcut($dest);
+    $s.TargetPath = $ahk
+    $s.WorkingDirectory = $source
+    $s.Arguments = "$source\$name.ahk"
+    $s.IconLocation = "$source\$name.exe, 0";
+    $s.Save()
+  }
+}
 
 
 function DoSetup() {
@@ -82,37 +110,50 @@ function DoSetup() {
   Set-ItemProperty $AdvancedKey ShowStatusBar 1
   Set-ItemProperty $AdvancedKey FullPath 1
 
+#startupAutoHotKeyScript WinWarden $env:userprofile\dotfiles\Windows\AutoHotKeyScripts\WinWarden WinWarden.gif
+
   Write-Host "Installing software:" -f Yellow
-  chocoToTaskbar vim "C:\Program Files (x86)\vim\vim80" "gvim.exe"
-  chocoToTaskbar conemu "C:\Program Files\ConEmu" "ConEmu64.exe"
-  chocoInstall poshgit
-  chocoInstall puretext
-  chocoToTaskbar tailblazer "C:\ProgramData\chocolatey\lib\tailblazer\tools" "TailBlazer.exe"
+#chocoToTaskbar vim "C:\Program Files (x86)\vim\vim80" "gvim.exe"
+#  chocoToTaskbar conemu "C:\Program Files\ConEmu" "ConEmu64.exe"
+#  chocoInstall poshgit
+#  chocoInstall puretext
+#  chocoToTaskbar tailblazer "C:\ProgramData\chocolatey\lib\tailblazer\tools" "TailBlazer.exe"
+#  chocoInstall autohotkey
 
   Remove-Item "C:\Program Files\ConEmu\ConEmu.xml" 
   linkFile Windows\ConEmu.xml "C:\Program Files\ConEmu\ConEmu.xml" 
 
 # Common settings
-  linkFolder . $env:userprofile\dotfiles
+  linkFolder $dotfiles $env:userprofile\dotfiles
 
-  linkFile .\vim\vimrc $env:userprofile\_vimrc
-  linkFile .\vim\vsvimrc $env:userprofile\_vsvimrc
-  linkFolder .\vim $env:userprofile\vimfiles
-  ensureFolder .\vim\backups
-  ensureFolder .\vim\bundle
-  ensureFolder .\vim\autoload
+#startupAutoHotKeyScript WinWarden $env:userprofile\dotfiles\Windows\AutoHotKeyScripts\WinWarden WinWarden.gif
 
-  Invoke-WebRequest https://tpo.pe/pathogen.vim -outfile ./vim/autoload/pathogen.vim
-  gitGet NERDtree ./vim/bundle/nerdtree https://github.com/scrooloose/nerdtree.git
-  gitGet Syntastic ./vim/bundle/syntastic https://github.com/vim-syntastic/syntastic.git
+  #linkFile .\vim\vimrc $env:userprofile\_vimrc
+  #linkFile .\vim\vsvimrc $env:userprofile\_vsvimrc
+  echo "Linkage should go here"
+  linkFile $dotfiles\vim\vimrcs\win.vimrc $env:userprofile\_vimrc
+  linkFile $dotfiles\vim\vimrcs\vsvim.vimrc $env:userprofile\_vsvimrc
+  echo "Linkage should go there"
+  linkFolder $dotfiles\vim $env:userprofile\vimfiles
+  ensureFolder $dotfiles\vim\backups
+  ensureFolder $dotfiles\vim\bundle
+  ensureFolder $dotfiles\vim\autoload
+
+  Invoke-WebRequest https://tpo.pe/pathogen.vim -outfile $dotfiles/vim/autoload/pathogen.vim
+  gitGet NERDtree $dotfiles/vim/bundle/nerdtree https://github.com/scrooloose/nerdtree.git
+  gitGet Syntastic $dotfiles/vim/bundle/syntastic https://github.com/vim-syntastic/syntastic.git
 
   git config --global core.editor vim
 }
 
 function DoPrereqs() {
+  if(-not(Test-Path "$env:userprofile\Documents\WindowsPowerShell")) {
+    linkFolder $dotfiles\Windows\WindowsPowerShell "$env:userprofile\Documents\WindowsPowerShell"
+  }
+
   if((Test-Path "$env:userprofile\Documents\WindowsPowerShell") -and (-not(Test-Path  "$env:userprofile\Documents\WindowsPowerShell_Backup"))) {
     mv "$env:userprofile\Documents\WindowsPowerShell" "$env:userprofile\Documents\WindowsPowerShell_Backup"
-    linkFolder Windows\WindowsPowerShell "$env:userprofile\Documents\WindowsPowerShell"
+    linkFolder $dotfiles\Windows\WindowsPowerShell "$env:userprofile\Documents\WindowsPowerShell"
   }
 
   if(-not (Test-Path "C:\ProgramData\chocolatey\bin")) {
